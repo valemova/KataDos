997146592740af7359a07651e98ce805541436c4
Vale Mova <Vale Mova@Marianne_Laptop>
2018-02-21 23:51:06 -0600

    prueba numero 1

---
+++ Examen/src/main/java/com/cenfotec/patrones/ExamenApplication.java
@@ -0,0 +1,12 @@
+package com.cenfotec.patrones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamenApplication.class, args);
+	}
+}
